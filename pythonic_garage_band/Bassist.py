8a9228464a27ec06a6f431f2eaae4b7be3262889
muhammadqasemtarboush1 <muhammadtarboush1@gmail.com>
2022-06-10 01:15:57 +0300

    improve: remove the dublicated functions and get it from the main class

--- pythonic_garage_band/Bassist.py
+++ pythonic_garage_band/Bassist.py
@@ -5,8 +5,3 @@ class Bassist(Musician):
     solo = 'bom bom buh bom'
     instrument = 'bass'
 
-    def __str__(self):
-        return f'My name is {self.name} and I play {self.instrument}'
-
-    def __repr__(self):
-        return f'Bassist instance. Name = {self.name}'
--- pythonic_garage_band/Guitarist.py
+++ pythonic_garage_band/Guitarist.py
@@ -6,8 +6,3 @@ class Guitarist(Musician):
     solo = 'face melting guitar solo'
     instrument = 'guitar'
 
-    def __str__(self):
-        return f'My name is {self.name} and I play {self.instrument}'
-
-    def __repr__(self):
-        return f'Guitarist instance. Name = {self.name}'
